8b6b6c018bad1a44aead948a1572b9aa28fa694a
Diego <diego.ruescas@gmail.com>
2023-05-12 06:46:24 -0300

    IO FileWriter - Escrita em  arquivos

---
+++ maratona-java/src/br/com/ruescas/devdojo/maratonajava/javacore/io/test/FileWriter01.java
@@ -0,0 +1,28 @@
+package br.com.ruescas.devdojo.maratonajava.javacore.io.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        // Usamos o try with source pois o FileWrither herda da classe closable
+        // Por padrao o FileWriter sobrescreve o arquivo caso exista. Para con-
+        // tinuarmos a escrita no arquivo adicionamos um segundo  parametro
+        // booleano para informar o append
+        try(FileWriter fw = new FileWriter(file, true)) {
+            //O FileWriter não possui um método para quebra de linha, mas para
+            // isso utilizamos o \n
+            fw.write("Olá Mundo!\nDiego\n");
+            // Quando estamos escrevendo no arquivo estamos pegando recursos do
+            // sistema operacional  e existe a possibilidade de por exemplo
+            // fecharmos o arquivo e  o sistema operacional não ter mandado todos
+            // esses dados para o arquivo e para isso utilizamos  o flush. Basica-
+            // mente estamos dizendo envia tudo  para o arquivo.
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
